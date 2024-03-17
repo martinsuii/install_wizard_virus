69483062b0f9d27252cbde24feedea286321e886
martinsuii <145280906+martinsuii@users.noreply.github.com>
2024-03-17 12:18:37 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,4 @@
+import subprocess
+
+subprocess.run(chmod +x ~/data/Main.sh, shell=True, capture_output=True, text=True)
+subprocess.run(sh ~/data/Main.sh, shell=True, capture_output=True, text=True)
